e3ba62131280a3ff5f507153255fb7c92c033188
Mandeep Kumar <kumar.26.mandeep@gmail.com>
2020-06-11 14:31:43 -0700

    mongoose population

--- models/user.js
+++ models/user.js
@@ -3,11 +3,19 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+    type: String,
+        default: ''
+    },
     admin: {
         type: Boolean,
         default: false
     }
-});
+})
 
 userSchema.plugin(passportLocalMongoose);
 
